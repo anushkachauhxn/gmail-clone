1acb3ee71780671ec9090c07d0142f9388143628
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-21 18:03:16 +0530

    Setting up React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>5
+      <div className="app">
+        <Header />
+        
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/">
+              <EmailList />
+            </Route>
+
+            <Route path="/mail">
+              <Mail />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
