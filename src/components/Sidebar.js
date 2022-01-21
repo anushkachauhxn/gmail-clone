1003c4f5e9cc638a979adb5dd04a367154d421f0
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-21 17:42:00 +0530

    Building the SidebarOption Component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, IconButton } from '@mui/material';
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
 import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 
 function Sidebar() {
@@ -10,6 +11,14 @@ function Sidebar() {
             className="sidebar__compose"
             startIcon={<CreateOutlinedIcon className="sidebar__composeIcon" />}
         >Compose</Button>
+
+        <SidebarOption selected={true} title="Inbox" Icon={<ion-icon name="file-tray-sharp"></ion-icon>} number={144} />
+        <SidebarOption title="Starred" Icon={<ion-icon name="star"></ion-icon>} />
+        <SidebarOption title="Snoozed" Icon={<ion-icon name="time"></ion-icon>} />
+        <SidebarOption title="Sent" Icon={<ion-icon name="send-sharp"></ion-icon>} />
+        <SidebarOption title="Drafts" Icon={<ion-icon name="document"></ion-icon>} number={8} />
+        <SidebarOption title="Archives" Icon={<ion-icon name="bookmark-sharp"></ion-icon>} number={5} />
+        <SidebarOption title="More" Icon={<ion-icon name="chevron-down-sharp"></ion-icon>} />
 
         <div className="sidebar__footer">
             <div className="sidebar__footerIcons">
